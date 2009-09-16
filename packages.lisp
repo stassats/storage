5f156d73754463501e9305fa2cb7bd1cb2fbbd06
Stas Boukarev <stassats@gmail.com>
2009-09-16 14:57:29 +0400

    Initial commit.

---
+++ packages.lisp
@@ -0,0 +1,48 @@
+;;; -*- Mode: Lisp -*-
+
+(defpackage #:movies
+  (:use #:cl)
+  (:export
+   #:add-person
+   #:add-view
+   #:where
+
+   #:*movies*
+   #:*views*
+   #:*persons*
+   #:*theatres*
+   #:*data-file*
+
+   #:person
+   #:movie
+   #:short-movie
+   #:feature-movie
+   #:theatre
+   #:view
+
+   #:date
+   #:theatre
+   #:animated-p
+   #:duration
+   #:documentary-p
+   #:directors
+   #:year
+   #:imdb-id
+   #:alternate-titles
+   #:died
+   #:born
+   #:imdb-id
+
+   #:save-data
+   #:load-data
+
+   #:lookup
+   #:list-all
+   #:count-))
+
+(defpackage #:imdb
+  (:use #:cl))
+
+(defpackage #:graphs
+  (:use #:cl #:adw-charting
+        #:movies))
