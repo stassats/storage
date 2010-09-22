319b6d978c5e1160600f09d1e5ca300bf6f7323c
Stas Boukarev <stassats@gmail.com>
2010-09-22 12:35:03 +0400

    Some old uncommitted changes to io-sbcl-2.lisp.

--- io-sbcl-2.lisp
+++ io-sbcl-2.lisp
@@ -20,14 +20,6 @@
                           (function (values) sb-alien:int))
    (st-stream-buffer-start stream)))
 
-(declaim (notinline stream-position))
-(defun stream-position (stream)
-  (st-stream-position stream))
-
-(declaim (notinline stream-length))
-(defun stream-length (stream)
-  (st-stream-length stream))
-
 (defconstant +buffer-size+ 8192)
 
 (deftype word ()
@@ -36,7 +28,8 @@
 (defstruct st-stream
   (fd nil :type word)
   (left 0 :type word)
-  (buffer-start (sb-sys:sap-int (sb-alien::%make-alien (* sb-vm:n-byte-bits +buffer-size+)))
+  (buffer-start (sb-sys:sap-int
+                 (sb-alien::%make-alien (* sb-vm:n-byte-bits +buffer-size+)))
                 :type word)
   (buffer-end 0 :type word)
   (buffer-position 0 :type word))
@@ -63,15 +56,25 @@
                      (space 0)))
   (declare (type sb-unix::unix-fd fd)
            (type word len))
-  (sb-alien:alien-funcall (sb-alien:extern-alien "read"
-                               (function sb-alien:int sb-alien:int
-                                         (* char) sb-alien:int)) fd
-                 buf len))
+  (sb-alien:alien-funcall
+   (sb-alien:extern-alien "read"
+                          (function sb-alien:int
+                                    sb-alien:int sb-alien:int sb-alien:int))
+   fd buf len))
+
+(defun unix-write (fd buf len)
+  (declare (type sb-unix::unix-fd fd)
+           (type word len))
+  (sb-alien:alien-funcall
+   (sb-alien:extern-alien "write"
+                          (function sb-alien:int
+                                    sb-alien:int sb-alien:int sb-alien:int))
+   fd buf len))
 
 (declaim (inline fill-buffer))
 (defun fill-buffer (stream offset)
   (let ((length (unix-read (st-stream-fd stream)
-                           (sb-sys:int-sap (+ (st-stream-buffer-start stream) offset))
+                           (+ (st-stream-buffer-start stream) offset)
                            (- +buffer-size+ offset))))
     (setf (st-stream-buffer-end stream)
           (+ (st-stream-buffer-start stream) (+ length offset)))
@@ -80,27 +83,33 @@
 
 (declaim (notinline refill-buffer))
 (defun refill-buffer (n stream)
-  (let ((left-n-bytes (- (st-stream-buffer-end stream)
-                         (st-stream-buffer-position stream))))
-    (when (> (- n left-n-bytes) (st-stream-left stream))
+  (declare (optimize speed (safety 0))
+           (type (integer 0 4)  n)
+           (st-stream stream))
+  (let ((left-n-bytes (sb-ext:truly-the word (- (st-stream-buffer-end stream)
+                                   (st-stream-buffer-position stream)))))
+    (when (> (the word (- n left-n-bytes))
+             (st-stream-left stream))
       (error "End of file ~a" stream))
     (unless (zerop left-n-bytes)
-      (setf (sb-sys:sap-ref-32 (sb-sys:int-sap (st-stream-buffer-start stream)) 0)
+      (setf (sb-sys:sap-ref-word (sb-sys:int-sap (st-stream-buffer-start stream)) 0)
             (n-sap-ref left-n-bytes (st-stream-buffer-position stream))))
     (fill-buffer stream left-n-bytes))
   (let ((start (st-stream-buffer-start stream)))
     (setf (st-stream-buffer-position stream)
-          (+ start n))))
+          (sb-ext:truly-the u32 (+ start n))))
+  t)
 
 (declaim (inline advance-stream))
 (defun advance-stream (n stream)
-  (declare (optimize speed)
+  (declare (optimize (space 0))
            (type word n)
            (type st-stream stream))
   (let* ((sap (st-stream-buffer-position stream))
          (new-sap (sb-ext:truly-the word (+ sap n))))
     (declare (word sap new-sap))
     (cond ((> new-sap (st-stream-buffer-end stream))
+           (- new-sap (st-stream-buffer-end stream))
            (refill-buffer n stream)
            (st-stream-buffer-start stream))
           (t
@@ -110,6 +119,13 @@
 
 (declaim (inline read-n-bytes))
 (defun read-n-bytes (n stream)
+  (declare (optimize (space 0))
+           (type word n))
+  (n-sap-ref n (advance-stream n stream)))
+
+(defun flush-buffer (stream))
+
+(defun write-n-bytes (n stream)
   (declare (optimize (space 0))
            (type word n))
   (n-sap-ref n (advance-stream n stream)))
@@ -134,12 +150,22 @@
               (sb-sys:fd-stream-fd ,fd-stream))))))))
 
 (defun test ()
-  (declare (optimize speed))
-  (storage-test::with-io-file (stream "/home/stas/new-2.db")
-    (loop repeat 3138465 count (evenp
-                                (storage-test::read-n-bytes 1 stream)))))
+  (declare (optimize speed (safety 0)))
+  (storage-test::with-io-file (stream "/home/stas/test-io-file")
+    (loop repeat 6276930
+          with count fixnum
+          if (evenp
+              (storage-test::read-n-bytes 1 stream))
+          do (setf count (sb-ext:truly-the fixnum (1+ count)))
+          finally (return count))))
 
 (defun test-2 ()
-  (declare (optimize speed))
-  (storage::with-io-file (stream "/home/stas/new-2.db")
-    (loop repeat 3138465 count (evenp (storage::read-n-bytes 1 stream)))))
+  (declare (optimize speed (safety 0)))
+  (storage::with-io-file (stream "/home/stas/test-io-file")
+    (loop repeat 6276930
+          with count fixnum
+          if (evenp
+              (storage::read-n-bytes 1 stream))
+          do (setf count (sb-ext:truly-the fixnum (1+ count)))
+          finally (return count))))
+
